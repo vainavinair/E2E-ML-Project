57045518b8e06002c95167e63749d307ff98730c
vainavinair <vainavinair2009558@gmail.com>
2025-02-18 23:06:36 +0530

    logs and exeption handling

---
+++ src/exception.py
@@ -0,0 +1,20 @@
+import sys
+from src.logger import logging
+
+def error_message_detail(error,erro_detail:sys):
+    _,_,exe_tb = erro_detail.exc_info()
+    error_message = " Error occured in python script name[{0}] line no [{1}] error message [{2}]".format(exe_tb.tb_frame.f_code.co_filename,exe_tb.tb_lineno,str(error))
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
+
+
+        
